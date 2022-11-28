8f25f2205055c47254f9d213d99302ea608e35a1
VelociRaptor <77036902+zignis@users.noreply.github.com>
2022-11-28 06:39:13 +0530

    Fix buffer deprecation warning

--- src/utils/decode/index.ts
+++ src/utils/decode/index.ts
@@ -4,7 +4,7 @@ const decodeHex = (hex: string): string | null => {
   let ref: number;
 
   if (hex && hex.length && hex.length % 2 === 0 && !hex.match(/[^\da-f]/)) {
-    const buffer = new Buffer(hex.length / 2);
+    const buffer = Buffer.alloc(hex.length / 2);
 
     for (i = j = 0, ref = hex.length; j < ref; i = j += 2) {
       buffer[i / 2] = Number.parseInt(hex.slice(i, +(i + 1) + 1 || 9e9), 16);
